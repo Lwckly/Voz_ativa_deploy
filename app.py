c48c3d0dabf6c88a2337b90227ad32b0ec61c8f9
Eric Efthimios <128000417+Lwckly@users.noreply.github.com>
2025-11-03 14:59:56 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ def transcribe():
     os.remove(tmp_path)
     return jsonify({"text": transcription.text})
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000)).    app.run(debug=True,host=0.0.0.0,port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
