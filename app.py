3ac9ed8aa3929765f410b7b4409ae41e873a6141
Eric Efthimios <128000417+Lwckly@users.noreply.github.com>
2025-11-04 20:46:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,32 +2,22 @@ import openai
 from flask import Flask, render_template, request, jsonify
 import tempfile, os
 from dotenv import load_dotenv
+from utils import audio_record,audio_transcribe
 
-load_dotenv()
 
 app = Flask(__name__)
-openai.api_key = os.getenv("api")
-#have to create .env to ignore api
-@app.route("/")
+
+@app.route("/", methods=["POST","GET"])
 def index():
+    if request.methods=="POST":
+        raw_audio = audio_record()
+        transcription= audio_transcribe(raw_audio)
+        
+    else:
     return render_template("index.html")
-
-@app.route("/transcribe", methods=["POST"])
-def transcribe():
-    audio = request.files["audio"]
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".webm") as tmp:
-        audio.save(tmp.name)
-        tmp_path = tmp.name
-
-    with open(tmp_path, "rb") as f:
-        transcription = openai.Audio.transcriptions.create(
-            model="whisper-1",
-            file=f
-        )
-    os.remove(tmp_path)
-    return jsonify({"text": transcription.text})
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+
 
