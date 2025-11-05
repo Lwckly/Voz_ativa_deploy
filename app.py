32ed6316f7dba97f494c0d3bf3feace3e1dc8d75
Eric Efthimios <128000417+Lwckly@users.noreply.github.com>
2025-11-05 12:54:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,16 +9,13 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST","GET","HEAD"])
 def index():
-    if request.methods=="POST":
-        raw_audio = audio_record()
-        transcription= audio_transcribe(raw_audio)
+    return render_template("index.html")
         
-    else:
-        return render_template("index.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+
 
 
 
