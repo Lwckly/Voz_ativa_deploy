416f51357fcefebf65be01a1131cc0b439ccfa63
Eric Efthimios <128000417+Lwckly@users.noreply.github.com>
2025-11-05 10:06:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from utils import audio_record,audio_transcribe
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST","GET"])
+@app.route("/", methods=["POST","GET","HEAD"])
 def index():
     if request.methods=="POST":
         raw_audio = audio_record()
@@ -19,6 +19,7 @@ def index():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+
 
 
 
