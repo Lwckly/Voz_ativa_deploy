d57bb581fdadc4a783f49245012c99a80207a291
Eric Efthimios <128000417+Lwckly@users.noreply.github.com>
2025-11-05 08:20:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,11 @@ def index():
         transcription= audio_transcribe(raw_audio)
         
     else:
-    return render_template("index.html")
+        return render_template("index.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+
 
 
